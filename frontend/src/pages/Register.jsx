c2ae73b0c1baf859368d15532d3c3a984675402d
ShalomiYogarathna <shalomiy@cigmatechnology.com>
2026-03-10 10:48:47 +0530

    initial project setup

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    console.log("Username:", username);
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    alert("Register button clicked");
+  };
+
+  return (
+    <div>
+      <h1>Register Page</h1>
+
+      <form onSubmit={handleRegister}>
+        <div>
+          <label>Username</label>
+          <br />
+          <input
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Email</label>
+          <br />
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Password</label>
+          <br />
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <button type="submit">Register</button>
+      </form>
+
+      <br />
+
+      <p>
+        Already have an account? <Link to="/">Login here</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Register;
