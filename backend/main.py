3880aa05349d8812959424dab96061fb85316ca6
ShalomiYogarathna <shalomiamanda725@gmail.com>
2026-03-11 12:59:39 +0530

    Setup FastAPI backend and connect Banana API

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,15 @@
-print("Another GitHub Desktop test")
+from fastapi import FastAPI
+import requests
 
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "Cake Quiz API is running"}
+
+
+@app.get("/banana")
+def get_banana_question():
+    response = requests.get("https://marcconrad.com/uob/banana/api.php")
+    return response.json()
